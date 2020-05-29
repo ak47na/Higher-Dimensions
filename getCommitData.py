6164a793d53e1e68fc1e9d026c06920f9838fcba
ak47na <54862378+ak47na@users.noreply.github.com>
2020-05-29 16:15:32 +0300

    Update getCommitData.py

--- getCommitData.py
+++ getCommitData.py
@@ -3,15 +3,19 @@
 from pydriller import RepositoryMining
 
 import datetime as dt
-import csv
 
 dt1 = dt.datetime(2020,1,1, 0, 0)
-dt2 = dt.datetime.now()
+dt2 = dt.datetime(2020,5,23, 0, 0)
+
+
+
 
 file1 = open("committerNames.txt", "w")
+File1 = open("committerTimes.txt", "w")
 file2 = open("CommitterAuthorFiles.txt","w") 
 file3 = open("FileNames.txt", "w")
 file4 = open("AuthorNames.txt", "w")
+File4 = open("AuthorTimes.txt", "w")
 commitData = open("commitData.txt", "w")
 
 committerNames = {}
@@ -21,19 +25,18 @@ fileIdx = 0
 authorNames = {}
 fileNames = {}
 
-Vis = {}
 
 for commit in RepositoryMining('https://github.com/eclipse/eclipse.jdt.core', since=dt1, to=dt2).traverse_commits():
     commitData.write(str(commit.hash) + '/\\' + str(commit.msg) + '/\\\n')
     file2.write(str(commit.committer.name) + "/\\" + str(commit.author.name) + "/\\" + str(commit.hash) + "\n")
     if not(commit.committer.name in committerNames):
       committerIdx += 1
-      committerNames[commit.committer.name] = committerIdx
-
+      committerNames[commit.committer.name] = []
+    committerNames[commit.committer.name].append(commit.committer_date)
     if not(commit.author.name in authorNames):
       authorIdx += 1
-      authorNames[commit.author.name] = authorIdx
-   
+      authorNames[commit.author.name] = []
+    authorNames[commit.author.name].append(commit.author_date)
     for modifFile in commit.modifications:
       fileList = modifFile.filename.split('.')
       if (fileList[-1] == 'jar' or fileList[-1] == 'java' or fileList[-1] == 'class'):
@@ -47,16 +50,26 @@ for commit in RepositoryMining('https://github.com/eclipse/eclipse.jdt.core', si
         
       file2.write("\n")
 
+idx = 0
 for Key in committerNames:
-  file1.write(str(Key) + ":" + str(committerNames[Key]) + "\n")
-
+  idx += 1
+  file1.write(str(Key) + "/\\" + str(idx) + "\n")
+  File1.write(str(Key) + "/\\" + str(len(committerNames[Key])) + "\n")
+  for el in committerNames[Key]:
+    File1.write(str(el) + '\n')
+File1.close()
+idx = 0
 for Key in authorNames:
-  file4.write(str(Key) + ":" + str(authorNames[Key]) + "\n")
-
+  file4.write(str(Key) + "/\\" + str(idx) + "\n")
+  File4.write(str(Key) + "/\\" + str(len(authorNames[Key])) + "\n")
+  for el in authorNames[Key]:
+    File4.write(str(el) + '\n')
+File4.close()
 for Key in fileNames:
-  file3.write(str(Key) + ":" + str(fileNames[Key]) + "\n")
+  file3.write(str(Key) + "/\\" + str(fileNames[Key]) + "\n")
 file1.close()
 file2.close()
 file3.close()
 file4.close()
 commitData.close()
+        
