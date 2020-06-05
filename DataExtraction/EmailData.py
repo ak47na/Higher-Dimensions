940259565fa61888d5c091f0d98761de96f13da9
ak47na <54862378+ak47na@users.noreply.github.com>
2020-06-05 11:37:17 +0300

    Update EmailData.py

--- DataExtraction/EmailData.py
+++ DataExtraction/EmailData.py
@@ -1,11 +1,17 @@
 import requests
+from datetime import datetime
+from datetime import timedelta
+
 
 pref = 'https://www.eclipse.org/lists/jdt-dev/msg'
 pageLinks = ['https://www.eclipse.org/lists/jdt-dev/threads.html']
 defaultLink = 'https://www.eclipse.org/lists/jdt-dev/thrd'
-detailsFile = open("details.txt", "wb")
+
 errorFile = open("errors.txt", "wb")
-edgeFile = open("msgEdges.txt", "w")
+edgeFile = open("emailMsgEdges.txt", "w")
+detailsFile = open("emailDetails.txt", "wb")
+namesFile = open("emailNames.txt", "wb")
+
 for i in range(2, 17):
   pageLinks.append(defaultLink + str(i) + '.html')
 
@@ -18,6 +24,8 @@ from bs4 import BeautifulSoup, NavigableString, Tag
 
 nrNodes = 0
 msgDex = {}
+msgDetails = {}
+names = {}
 
 def CheckPage(pageBody):
   global nrNodes
@@ -31,25 +39,77 @@ def CheckPage(pageBody):
         msgDex[msg] = (lst.find('em'), nrNodes)
       else:
         if (msgDex[msg][0] != lst.find('em')):
-          print(msgDex[msg], lst.find('em'))
           msgDex[msg] = (lst.find('em'), msgDex[msg][1])
-        print(msgDex[msg], lst.find('em'))
       
   print(len(pageBody.find_all('em')), nrNodes)
 
 def addEdge(u, v):
    if u[0] != -1 and v[0] != -1:
      edgeFile.write(str(u[0]) + '/\\' + str(v[0]))
+def getDateFromList(s):
+  idx = 0
+  sLen = len(s)
+  if ',' in s:
+    while idx < sLen and s[idx] != ',':
+      idx += 1
+    idx += 2 #pass ', '
+  else:
+    while not isDigit(s[idx]):
+      idx += 1
+  dateStr = ''
+  if ('-' in s) or ('+' in s):
+    while idx < sLen and s[idx] != '-' and s[idx] != '+':
+      dateStr += s[idx]
+      idx += 1
+    dateStr = dateStr[:-1]
+    Date = datetime.strptime(dateStr, '%d %b %Y %H:%M:%S') + timedelta(hours = int(s[idx+1:idx+3]), minutes = int(s[idx+3:idx+5]))
+  else:
+    if not('GMT' in s):
+      print(s)
+      s.replace('UTC', 'GMT')
+    while (idx < sLen - 2) and (s[idx] != 'G' or s[idx + 1] != 'M' or s[idx + 2] != 'T'):
+      dateStr += s[idx]
+      idx += 1
+    idx = len(dateStr) - 1
+    while (dateStr[idx] == ' '):
+      dateStr = dateStr[:-1]
+      -- idx
+    Date = datetime.strptime(dateStr, '%d %b %Y %H:%M:%S')
+  
+  return Date
+
+def isDigit(a):
+  return (ord(a) <= ord('9') and ord(a) >= ord('0'))
+def isLetter(a):
+  return (ord(a) <= ord('z') and ord(a) >= ord('a')) or (ord(a) <= ord('Z') and ord(a) >= ord('A'))
+
+def getNameFromList(s):
+  idx = 0
+  sLen = len(s)
+  name = ''
+  s = s.replace('<li><em>From</em>:', '')
+  while s[idx] != '&':
+    if isLetter(s[idx]):
+      name += s[idx]
+    idx += 1
+  return name
+def getIDFromEmail(s):
+  idx = 0
+  sLen = len(s)
+  name = ''
+  while s[idx] != '@':
+    name += s[idx]
+    idx += 1
+  return name
 
 for pageLink in pageLinks:
-  msgDex = {}
   nrNodes = 0
   BS = BeautifulSoup(getPageContent(pageLink), 'html.parser')
   CheckPage(BS.body)
   
   
 for key in msgDex:
-  #print(pref + key + '.html') 
+  #print(pref + key + '.html')
   BSi = BeautifulSoup(getPageContent(pref + key + '.html'), 'html.parser')
   #print(BSi.find_all(text=lambda text: (isinstance(text, Comment)) and 'X-Date' in text))
   allLists = BSi.find_all('li')
@@ -71,11 +131,13 @@ for key in msgDex:
         cnt += 1
     if cnt == 2:
       break
-  if not 'email' in msgDetails[key]:
-    print(key)
-  else:
-    detailsFile.write((str(msgDetails[key]['name']) +'/\\' + str(msgDetails[key]['email']) + '/\\' + str(msgDetails[key]['date']) + '\n').encode())
+  
+  detailsFile.write((str(msgDetails[key]['name']) +'/\\' + str(msgDetails[key]['email']) + '/\\' + str(msgDetails[key]['date']) + '\n').encode())
+  if not msgDetails[key]['name'] in names:
+    names[msgDetails[key]['name']] = msgDetails[key]['email']
+    namesFile.write((str(msgDetails[key]['name']) + '/\\' + str(msgDetails[key]['email']) + '\n').encode())
 
+namesFile.close()
 detailsFile.close()
 errorFile.close()
 edgeFile.close()
