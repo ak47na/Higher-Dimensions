9a13cac7d2cd804d7a71bdd924e125066229f473
ak47na <54862378+ak47na@users.noreply.github.com>
2020-06-15 10:22:53 +0300

    Update temporalGraph.py

--- GraphConstruction/temporalGraph.py
+++ GraphConstruction/temporalGraph.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import Sample
 # from datetime import timedelta
 
-
+Label = {}
 Adj = {}
 msgDict = {}
 nrNodes = 0
@@ -15,9 +15,11 @@ Edges = []
 
 edges = []
 def addHuman(name):
+    name = mailID.getIdentity(name)
     global nrNodes
     if not (name in humanDict):
         nrNodes += 1
+        Label[nrNodes] = name
         humanDict[name] = nrNodes
         Adj[nrNodes] = []
 
@@ -27,13 +29,16 @@ def addEdge(u, v):
     b = msgDict[v][0]
     addHuman(a)
     addHuman(b)
-    if not (msgDict[u][1] in timeDict):
-        timeDict[msgDict[u][1]] = True
-    edges.append((humanDict[a], humanDict[b], msgDict[u][1]))
+    if a == b:
+        return
+    if not (msgDict[v][1] in timeDict):
+        timeDict[msgDict[v][1]] = True
+
+    edges.append((humanDict[a], humanDict[b], msgDict[v][1]))
 
 def readMsgDetails():
     nrM = 0
-    detailsFile = open("\\TxtDataInUse\\msgDetails.txt", "r")
+    detailsFile = open("\\msgDetails.txt", "r")
     while True:
         crtL = detailsFile.readline()
         if not crtL:
@@ -42,11 +47,10 @@ def readMsgDetails():
         #msgKey/\name/\email/\date
         lst = crtL.split('/\\')
         if len(lst) != 4:
-            print(lst)
             exit()
         nrM += 1
         msgEmail = lst[2].replace(' ', '')
-        msgEmail = mailID.removeStrings(msgEmail.replace('\n', '')).replace(' ', '')
+        msgEmail = mailID.purifyEmail(mailID.removeStrings(msgEmail.replace('\n', '')))
         msgDate = datetime.strptime(lst[3], '%Y-%m-%d %H:%M:%S')
         msgDict[lst[0]] = (msgEmail, msgDate)
     detailsFile.close()
@@ -55,7 +59,7 @@ def readMsgDetails():
 def readMsgEdges():
     errors = 0
     invalidMsg = {}
-    edgeFile = open("\\TxtDataInUse\\msgEdges.txt", "r")
+    edgeFile = open("\\msgEdges.txt", "r")
     while True:
         crtL = edgeFile.readline()
         if not crtL:
@@ -74,38 +78,41 @@ def readMsgEdges():
         if lst[0] in msgDict and lst[1] in msgDict:
             addEdge(lst[1], lst[0])
     edgeFile.close()
-    print(errors)
 
 def normTime():
     global nrLayers
     timeList = []
+    yearDict = {}
     for t in timeDict:
         timeList.append(t)
+        yearDict[t.year] = True
     timeList = sorted(timeList)
 
     nrLayers = len(timeList)
+    #print(timeList[0].year, timeList[nrLayers - 1])
+
     for i in range(nrLayers):
-        timeDict[timeList[i]] = i + 1
+        timeDict[timeList[i]] = i
+    #nrLayers = timeDict[timeList[nrLayers - 1]]
+
 
 def getEdges():
     from Edge import myEdge
     for edge in edges:
-        Edges.append(myEdge(edge[0], timeDict[edge[2]], edge[1], timeDict[edge[2]], 0))
-        Adj[edge[0]].append((edge[1], timeDict[edge[2]]))
+            Edges.append(myEdge(edge[0], timeDict[edge[2]], edge[1], timeDict[edge[2]], 1))
+            Adj[edge[0]].append((edge[1], timeDict[edge[2]]))
 
 mailID.init()
 readMsgDetails()
 readMsgEdges()
 normTime()
 getEdges()
-nodes = []
-nodeSample = Sample.sampleOfNodes(nrNodes, 40)
-for node in nodeSample:
-    nodes.append((node, Adj[node]))
-s = Sample.Sample(nrLayers, Sample.sampleFromNodes(nodes), Edges)
+nodes, sampleEdges = Sample.sampleNodesFromEdges(Edges, 1)
+
+s = Sample.Sample(nrLayers, nodes, sampleEdges, Label)
 print(s.getNrNodes(), s.getNrEdges(), s.getNrLayers())
 s.addOrdinalAliasEdges()
 print(s.getNrNodes(), s.getNrEdges(), s.getNrLayers())
-s.createEdgesFile("\\temporalEdges.txt")
+s.createEdgesFile("\\muxViz-master\\data\\temporalGraph\\temporalEdges.txt")
 Sample.createLayoutFile("\\muxViz-master\\data\\temporalGraph\\B.txt", s.getNrNodes(), False)
-Sample.createLayoutFile("\\muxViz-master\\data\\temporalGraph\\C.txt", s.getNrLayers(), True)
+# Sample.createLayoutFile("D:\\Ak_work2019-2020\\muxViz-master\\data\\temporalGraph\\C.txt", s.getNrLayers(), True)
