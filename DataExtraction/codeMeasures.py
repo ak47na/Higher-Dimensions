a77e457540e36608ea712eb85d6390408b7bb871
ak47na <54862378+ak47na@users.noreply.github.com>
2020-06-16 14:52:32 +0300

    Update codeMeasures.py

--- DataExtraction/codeMeasures.py
+++ DataExtraction/codeMeasures.py
@@ -37,16 +37,16 @@ class Complexity(ProcessMetric):
 
                 filepath = renamed_files.get(modified_file.new_path, modified_file.new_path)
                 if modified_file.new_path == None:
-                  continue
+                    continue
                 fileExt = modified_file.new_path.rsplit('.', 1)[-1].lower()
                 if fileExt != 'java':
                     continue
                 if modified_file.change_type == ModificationType.RENAME:
                     renamed_files[modified_file.old_path] = filepath
-                
+
                 complexity = modified_file.complexity
                 if complexity == None:
-                  print(modified_file.new_path, modified_file.old_path)
+                    print(modified_file.new_path, modified_file.old_path)
                 self.files.setdefault(filepath, []).append(complexity)
 
     def count(self):
@@ -82,23 +82,106 @@ class Complexity(ProcessMetric):
 
         return avg_count
 
+class NLOC(ProcessMetric):
+    """
+    This class is responsible to implement the NLOC metric for a
+    file.
+    It allows to count for the:
+    * last value of NLOC;
+    * maximum NLOC;
+    * average NLOC.
+    """
+
+    def __init__(self, path_to_repo: str,
+                 since=None,
+                 to=None,
+                 from_commit: str = None,
+                 to_commit: str = None):
+
+        super().__init__(path_to_repo, since=since, to=to, from_commit=from_commit, to_commit=to_commit)
+        self._initialize()
+    def _initialize(self):
+        renamed_files = {}
+        self.files = {}
+     
+        # traverse commits from oldest to newest
+        for commit in self.repo_miner.traverse_commits():
+            for modified_file in commit.modifications:
+                filepath = renamed_files.get(modified_file.new_path, modified_file.new_path)
+                if modified_file.new_path == None:
+                    continue
+                fileExt = modified_file.new_path.rsplit('.', 1)[-1].lower()
+                if fileExt != 'java':
+                    continue
+                if modified_file.change_type == ModificationType.RENAME:
+                    renamed_files[modified_file.old_path] = filepath
+                nloc = modified_file.nloc
+                if nloc == None:
+                    print(modified_file.new_path, modified_file.old_path, modified_file.added, modified_file.removed)
+                else:
+                    self.files.setdefault(filepath, []).append(nloc)
+                 
+
+
+    def count(self):
+        """
+        Return the last nloc of the modified file.
+        :return: int nloc
+        """
+        count = dict()
+        for path, nloc in self.files.items():
+            count[path] = nloc[-1]
+
+        return count
+
+    def max(self):
+        """
+        Return the maximum nloc for each modified file.
+        :return: int max nloc
+        """
+        max_count = dict()
+        for path, nloc in self.files.items():
+            max_count[path] = max(nloc)
+
+        return max_count
+
+    def avg(self):
+        """
+        Return the average complexity for each modified file.
+        :return: int avg complexity rounded off to the nearest integer
+        """
+        avg_count = dict()
+        for path, nloc in self.files.items():
+            avg_count[path] = round(statistics.mean(nloc))
+        return avg_count
+    def sumC(self):
+        sum_count = dict()
+        for path, nloc in self.files.items():
+            sum_count[path] = sum(nloc)
+        return sum_count
+
+
 import datetime as dt
 
-dt1 = dt.datetime(2020,1,1, 0, 0)
-dt2 = dt.datetime(2020,5,23, 0, 0)
+dt1 = dt.datetime(2020, 1, 1, 0, 0)
+dt2 = dt.datetime(2020, 5, 23, 0, 0)
 
 measure = Complexity(path_to_repo='https://github.com/eclipse/eclipse.jdt.core',
-                      since=dt1,
-                      to=dt2).count()
+                     since=dt1,
+                     to=dt2).count()
 measure2 = CodeChurn(path_to_repo='https://github.com/eclipse/eclipse.jdt.core',
+                     since=dt1,
+                     to=dt2).count()
+measure3 = NLOC(path_to_repo='https://github.com/eclipse/eclipse.jdt.core',
                       since=dt1,
                       to=dt2).count()
-measure3 = LinesCount(path_to_repo='https://github.com/eclipse/eclipse.jdt.core',
+measure4 = NLOC(path_to_repo='https://github.com/eclipse/eclipse.jdt.core',
                       since=dt1,
-                      to=dt2).count()
+                      to=dt2).sumC()
 
-fileData = open("/dataFiles.txt", "w")
+fileData = open("/FileMetrics.txt", "w")
 for file in measure:
-    values = str(measure[file]) + '/\\' + str(measure2[file]) + '/\\' + str(measure3[file])
+    values = str(measure[file]) + '/\\' + str(measure2[file]) + '/\\' +\
+             str(measure3[file]) + '/\\' + str(measure4[file])
     fileData.write(str(file) + '/\\' + values + '\n')
 fileData.close()
